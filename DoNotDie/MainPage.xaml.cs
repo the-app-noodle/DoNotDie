9c8366ae6133339d1d81b709e6c3e3702c99e133
William Scarbrough <scarbroughw@yahoo.com>
2019-04-29 23:45:41 -0400

    init repo
    The base program has been written and is functioning.  Pick one of the three buttons.  Two of them are safe, one of them is a "bomb".  When a safe button is clicked, player earns a point.  When the "bomb" is clicked, the game ends and points earned is displayed.

---
+++ DoNotDie/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DoNotDie
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        static string bomb = new Random().Next(1, 4).ToString();
+        int pts = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void ButtonClicked(Object sender, EventArgs e) {
+
+            Button button = sender as Button;
+
+            // This is a game over scenario
+            if (button.Text == bomb)
+            {
+                await DisplayAlert("GAME OVER", "Sorry, you have clicked on the bomb and died \n Points: "+pts, "Retry");
+                bomb = new Random().Next(1, 4).ToString();
+
+            }
+
+            else {
+                pts += 1;
+                bomb = new Random().Next(1, 4).ToString();
+            }
+        
+        }
+    }
+}
